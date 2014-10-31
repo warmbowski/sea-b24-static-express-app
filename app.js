f357310611c2089c29ba27f4be1920b0f9f9a014
Paul Laskowski <pelaskowski@gmail.com>
2014-10-30 23:51:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use('/styles', express.static(__dirname + '/styles'));
+
+app.get('/', function(req, res) {
+  res.sendfile("./index.html");
+});
+
+app.get('/about', function(req, res) {
+  res.sendfile("./about.html");
+});
+
+app.get('/api', function(req, res) {
+  res.send({screenName:"Warmbowski",age:46});
+});
+
+app.listen(port);
+console.log("Server running on port " + port);
